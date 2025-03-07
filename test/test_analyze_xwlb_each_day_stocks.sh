67aa4d303382ef28b5813a91955ddab048dc4e49
L.L <lhyrcp@gmail.com>
2025-03-07 09:28:39 +0800

    fix script path reference.

--- test/test_analyze_xwlb_each_day_stocks.sh
+++ test/test_analyze_xwlb_each_day_stocks.sh
@@ -1,4 +1,6 @@
-python test_analyze_xwlb_each_day.py \
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+python ${SCRIPT_DIR}/test_analyze_xwlb_each_day.py \
     --prompt "下一行之后是中央电视台新闻联播的内容。将新闻中除了政治领域以外的和以往不同的表述(不要从文章中推断，而是使用已有的知识)总结一下。根据总结，请列举最受影响的A股上市公司。请以\"股票代码，股票名称，一句话原因\"这个形式去总结。" \
     --llm_use_case xwlb_each_day_stocks \
     $@
